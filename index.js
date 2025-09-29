42c784cfd8aa954bca4e2f4ca9343e0138433b8d
MORVILxv <morvilxv@gmail.com>
2025-09-29 07:18:25 +0000

    change2

---
+++ index.js
@@ -0,0 +1,59 @@
+import { createServer } from "node:http";
+import { readFileSync } from "node:fs";
+import { URL } from "node:url";
+
+const index_html = readFileSync("static/index.html");
+
+const pathConfigs = [
+    {
+        path: "/",
+        allowed_methods: ["GET"],
+        handler: (req, res) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(index_html);
+        },
+    },
+    {
+        path: "/hello",
+        allowed_methods: ["GET"],
+        handler: (req, res) => {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("hello world!\n");
+        },
+    },
+];
+
+
+
+
+// Create a HTTP server
+const server = createServer((req, res) => {
+    const request_url = new URL(`http://${host}${req.url}`);
+    const path = request_url.pathname;
+    console.log(`Request: ${req.method} ${path}`);
+
+    for (let config of pathConfigs) {
+        if (path === config.path) {
+        if (config.allowed_methods.includes(req.method)) {
+            config.handler(req, res);
+        } else {
+            res.writeHead(405, { "Content-Type": "text/plain" });
+            res.end("Method not allowed\n");        
+        }
+        break;
+        }
+    }
+
+    if (!res.writableEnded) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Site not found!\n");
+    }
+})
+
+const port = 8000;
+const host = "localhost";
+
+// start
+server.listen(port, host, () => {
+    console.log(`Server listening on http://${host}:${port}`);
+})
